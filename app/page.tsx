c4dd3b184e97a0eb5e20fb6192d52c230b85121b
ferdoushasanpr <ferdoushasanpr@gmail.com>
2026-02-20 15:32:17 +0600

    feat: MenuCard Component Added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/hero-section";
+import LatestMenu from "@/components/latest-menu/latest-menu";
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
+      <LatestMenu />
     </div>
   );
 }
