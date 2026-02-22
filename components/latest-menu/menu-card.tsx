c20f7c5c01e327cdead977c778833e80e03a7a8b
ferdoushasanpr <ferdoushasanpr@gmail.com>
2026-02-22 21:39:40 +0600

    fix: MenuCard Border and Paragraph Color Fixed

--- components/latest-menu/menu-card.tsx
+++ components/latest-menu/menu-card.tsx
@@ -18,14 +18,14 @@ export default function MenuCard({
   recommendation,
 }: MenuCardProps): React.ReactNode {
   return (
-    <div className="relative max-w-sm border border-[#CBB790] rounded-2xl overflow-hidden">
+    <div className="relative max-w-sm border border-[#CBB790]/20 rounded-2xl overflow-hidden">
       <Image src={image} alt={title} />
       <h6 className="absolute top-6 right-6 z-20 text-[#F4AF25] font-bold font-work text-md">
         ${price}
       </h6>
       <div className="bg-[#231D10] p-6">
         <h5 className="font-bold font-work text-xl text-white">{title}</h5>
-        <p className="font-work text-sm text-[#CBB790] py-2">{desc}</p>
+        <p className="font-work text-sm text-[#CBB790]/70 py-2">{desc}</p>
         <div className="flex gap-2 py-2">
           {recommendationIcon}
           <h6 className="uppercase font-bold font-work text-[#CBB790] text-sm">
