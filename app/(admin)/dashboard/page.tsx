0e88794df28e5d4c460795212f2be9fc26944c61
ferdoushasanpr <ferdoushasanpr@gmail.com>
2026-02-26 22:10:50 +0600

    feat: Sidebar Component Added

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 
+import Sidebar from "@/components/sidebar/sidebar";
+
 const DashboardPage = (): React.ReactNode => {
   return (
     <div className="flex min-h-screen bg-[#120f0a] text-stone-200 font-sans">
+      <Sidebar />
+
+      {/* --- Main Content --- */}
       <main className="flex-1 p-8 overflow-y-auto"></main>
     </div>
   );
---
+++ components/sidebar/sidebar.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import Image from "next/image";
+import {
+  CalendarDays,
+  LayoutDashboard,
+  LogOut,
+  PlusCircle,
+  Settings,
+  UtensilsCrossed,
+} from "lucide-react";
+import NavIcon from "@/assets/icons/Icon.png";
+import NavItem from "./sidebar-navitem";
+
+export default function Sidebar(): React.ReactNode {
+  return (
+    <aside className="w-64 bg-[#1a1610] flex flex-col p-6 border-r border-stone-800/50">
+      <div className="flex items-center gap-3 mb-10">
+        <Image src={NavIcon} alt="Gourmet Haven Logo" width={18} height={24} />
+        <div>
+          <h1 className="font-bold font-work text-lg leading-tight">
+            GourmetAdmin
+          </h1>
+          <p className="text-[10px] font-work uppercase tracking-widest text-stone-500">
+            Management Suite
+          </p>
+        </div>
+      </div>
+
+      <nav className="flex-1 space-y-2">
+        <NavItem
+          icon={<LayoutDashboard size={20} />}
+          label="Dashboard"
+          active
+        />
+        <NavItem icon={<PlusCircle size={20} />} label="Add Food" />
+        <NavItem icon={<UtensilsCrossed size={20} />} label="Food List" />
+        <NavItem icon={<CalendarDays size={20} />} label="Reservations" />
+      </nav>
+
+      <div className="mt-auto space-y-6">
+        <NavItem icon={<Settings size={20} />} label="Settings" />
+
+        <button className="w-full bg-[#2a1a15] text-red-400 py-3 rounded-xl flex items-center justify-center gap-2 font-medium font-work hover:bg-red-950/30 transition-colors">
+          <LogOut size={18} />
+          Logout
+        </button>
+      </div>
+    </aside>
+  );
+}
