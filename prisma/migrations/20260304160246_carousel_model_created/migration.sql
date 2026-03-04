4c883a92efe5484f8d1b9513ab53de7fe6ccaa20
ferdoushasanpr <ferdoushasanpr@gmail.com>
2026-03-04 22:03:42 +0600

    feat: Carousel Model Added to Prisma Schema

---
+++ prisma/migrations/20260304160246_carousel_model_created/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Carousel" (
+    "id" SERIAL NOT NULL,
+    "image" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Carousel_pkey" PRIMARY KEY ("id")
+);
