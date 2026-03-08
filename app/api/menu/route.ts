968b52fb5a9a86bafbca7c529f15f84a30997b4a
ferdoushasanpr <ferdoushasanpr@gmail.com>
2026-03-08 22:59:50 +0600

    feat: Menu Route POST Method Added

---
+++ app/api/menu/route.ts
@@ -0,0 +1,40 @@
+import { prisma } from "@/lib/prisma";
+
+export const POST = async (req: Request) => {
+  try {
+    const { name, cat, price, desc, image, status } = await req.json();
+
+    if (!name || !cat || !price || !desc || !image || !status) {
+      return Response.json(
+        { error: "Missing required fields" },
+        { status: 400 },
+      );
+    }
+
+    const response = await prisma.food.create({
+      data: {
+        name,
+        cat,
+        price,
+        desc,
+        image,
+        status,
+      },
+    });
+
+    return Response.json(
+      {
+        success: true,
+        message: "Successfully Food Added.",
+        data: response,
+      },
+      { status: 201 },
+    );
+  } catch (error) {
+    console.error("Menu Error:", error);
+
+    return Response.json({ error: "Internal Server Error" }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
