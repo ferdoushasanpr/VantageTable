803476a49dff64d14a1cab6def9f98271973d023
ferdoushasanpr <ferdoushasanpr@gmail.com>
2026-04-05 22:34:30 +0600

    mod: Force Dynamic Added to Reservation Component

--- components/reservation.tsx
+++ components/reservation.tsx
@@ -4,6 +4,8 @@ import { reservationInputHandler } from "@/actions/reserve";
 import React, { useActionState } from "react";
 import SubmitButton from "./buttons/submit-button";
 
+export const dynamic = "force-dynamic";
+
 export default function Reservation(): React.ReactNode {
   const [state, formAction] = useActionState(reservationInputHandler, {
     error: "",
