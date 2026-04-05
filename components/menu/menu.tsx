d1272f6e4ac79a97603732ace93d7406da7ead76
ferdoushasanpr <ferdoushasanpr@gmail.com>
2026-04-05 21:57:59 +0600

    mod: Force Dynamic Added to Menu Component

--- components/menu/menu.tsx
+++ components/menu/menu.tsx
@@ -2,6 +2,8 @@ import { fetchMenu } from "@/actions/menu";
 import React from "react";
 import MenuClient from "./menu-client";
 
+export const dynamic = "force-dynamic";
+
 const Menu = async (): Promise<React.ReactNode> => {
   const data = await fetchMenu();
   const menuItems = data.data;
